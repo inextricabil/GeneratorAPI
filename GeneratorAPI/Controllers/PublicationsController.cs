0434506bdfe6b1f7fcb0eca0fd72c5f53314ce66
Serban Boghiu <pincellodev14@outlook.com>
2018-04-26 10:05:07 +0200

    Initial setup

---
+++ GeneratorAPI/Controllers/PublicationsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Terminator.BusinessLayer.Publications;
+
+namespace Terminator.Controllers
+{
+    public class PublicationsController : ApiController
+    {
+        // GET: Publications
+        [HttpGet]
+        public List<Publication> Index(int id)
+        {
+            var publicationsConfiguration = new PublicationConfiguration();
+            var generator = new PublicationGenerator();
+           return generator.Generate(publicationsConfiguration, id);
+        }
+
+
+        [HttpGet]
+        public List<Publication> Index2( )
+        {
+            var publicationsConfiguration = new PublicationConfiguration();
+            var generator = new PublicationGenerator();
+           return generator.Generate(publicationsConfiguration, 30000);
+        }
+    }
+}
---
+++ GeneratorAPI/Controllers/SubscriptionsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Terminator.BusinessLayer.Subscription;
+
+namespace Terminator.Controllers
+{
+    public class SubscriptionsController : ApiController
+    {
+        [HttpGet]
+        public List<Subscription> Index()
+        {
+            var publicationsConfiguration = new SubscriptionConfiguration();
+            var generator = new SubscriptionGenerator();
+            return generator.Generate(publicationsConfiguration, 30000);
+        }
+
+        [HttpGet]
+        public List<Subscription> Index(int id)
+        {
+            var publicationsConfiguration = new SubscriptionConfiguration();
+            var generator = new SubscriptionGenerator();
+            return generator.Generate(publicationsConfiguration, id);
+        }
+    }
+}
